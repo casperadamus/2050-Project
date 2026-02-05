a6a60e3189640ce2af1ea1cff0f0745f864dcaaa
Mert Balik <balikmerttr@gmail.com>
2026-02-04 22:45:59 -0500

    Complete Classes w/Docstrings

--- main.py
+++ main.py
@@ -1,39 +1,88 @@
+gradeDict = {'A':4.0,'A-':3.7,'B+':3.3,'B':3.0,'B-':2.7,'C+':2.3,'C': 2.0,'C-': 1.7,'D':1.0,'F': 0.0} #Letter grade to numbers
+
 class Course: #Mert
     """Class blueprint for a course."""
     def __init__(self, code, creds, students=[]):
-        """Initializes 3 public variables for """
+        """Initializes 3 public variables for course"""
         self.course_code = code #string (e.g. "CSE1010")
         self.CREDITS = creds #final integer (shouldn't change)
         self.students = students #List (Student objects)
 
     def add_student(self, student):
-        """Adds a student object to the students list of the course."""
-        for i in self.students:
-            if i!=student:
+        """Adds a student object to the students list"""
+        if student not in self.students:
                 self.students.append(student)
 
     def get_student_count(self):
-        """Return total amount of students in the course."""
+        """Return total amount of students"""
         return len(self.students)
-"""
-class Student: #Mert (In Progress)
-    def __init__(self,id,name,courses):
+
+class Student: #Mert
+    def __init__(self,id,name,courses={}):
         self.student_id = id #string
         self.name = name #string
         self.courses = courses #dict
     
     def enroll(self, course, grade):
-        #— enrolls the student in a course with the given grade and updates the course roster
+        """Student enroll w/ grade in course; Updates course.students list"""
+        self.courses[course] = grade
+        course.add_student(self)
 
     def update_grade(self, course, grade):
-        #modify the student grade for a particular course
+        """Modify grade of course obj"""
+        self.enroll(course,grade)
 
     def calculate_gpa(self):
-        #modify the student grade for a particular course
+        """Returns GPA of student's courses"""
+        pts=0
+        creds=0
+        for course, grade in self.courses:
+            pts+=gradeDict.get(grade, 0)*course.CREDITS
+            creds+=course.CREDITS
+        return 0 if creds == 0 else round(pts/creds)
+
 
     def get_courses(self):
-        #returns a list of course objects taken by the student.
+        """Return list of students courses objs"""
+        return [course for course, v in self.courses]
 
     def get_course_info(self):
-        #returns a structured summary of all enrollments, including course code, grade, and credits.
-"""  
+        """Return set including students courses code, grade, credits."""
+        return {(k.course_code, v, k.CREDITS) for k,v in self.courses}
+    
+class University: #Mert
+    def __init__(self,id,name,courses=dict()):
+        students = {} #dict maps student_id -> Student object
+        courses = {} #dict maps course_code -> Course object
+
+    def add_course(self,course_code,credits):
+        """Add new course from code"""
+        if course_code not in self.courses:
+            self.courses[course_code] = Course(course_code, credits)
+        return self.courses[course_code]
+
+    def add_student(self, student_id, name):
+        """Add new student from id"""
+        if student_id not in self.students:
+            self.students[student_id] = Student(student_id, name)
+        return self.students[student_id]
+    
+    def get_student(self, student_id):
+        """Return student obj from id"""
+        return self.students.get(student_id)
+
+    def get_course(self, course_code):
+        """Return course obj from code"""
+        return self.courses.get(course_code)
+        
+    def get_course_enrollment(self, course_code):
+        """return student count from code"""
+        course = self.courses.get(course_code)
+        if course:
+            return course.get_student_count()
+
+    def get_students_in_course(self, course_code):
+        """Return student objs from code"""
+        course = self.courses.get(course_code)
+        if course:
+            return course.students
