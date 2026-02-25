7fca7462946cd13757818dfa3a6204b05e8c9933
casperadamus <adamus.casper@gmail.com>
2026-02-25 10:57:06 -0500

    small tweaks

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ gradeDict = {'A':4.0,'A-':3.7,'B+':3.3,'B':3.0,'B-':2.7,'C+':2.3,'C': 2.0,'C-': 
 
 class Course: #Mert
     """Class blueprint for a course."""
-    def __init__(self, code, creds, students=[]):
+    def __init__(self, code, creds, students=None):
         """Initializes 3 public variables for course"""
         self.course_code = code #string (e.g. "CSE1010")
         self.CREDITS = creds #final integer (shouldn't change)
-        self.students = students #List (Student objects)
+        self.students = students if students is not None else [] #List (Student objects)
 
     def add_student(self, student):
         """Adds a student object to the students list"""
@@ -21,10 +21,10 @@ class Course: #Mert
         return f"{self.course_code} ({self.CREDITS} credits)"
 
 class Student: #Mert
-    def __init__(self,id,name,courses={}):
+    def __init__(self,id,name,courses= None):
         self.student_id = id #string
         self.name = name #string
-        self.courses = courses #dict
+        self.courses = courses if courses is not None else {} #dict  def enroll(self, course, grade):
     
     def enroll(self, course, grade):
         """Student enroll w/ grade in course; Updates course.students list"""
@@ -45,16 +45,21 @@ class Student: #Mert
         return 0 if creds == 0 else round(pts/creds)
 
 
-    def get_courses(self):a
+    def get_courses(self):
         """Return list of students courses objs"""
         return [course for course, v in self.courses]
 
     def get_course_info(self):
         """Return set including students courses code, grade, credits."""
         return {(k.course_code, v, k.CREDITS) for k,v in self.courses}
+
+    def __str__(self):
+        return f"{self.name} ({self.student_id}) GPA: {self.calculate_gpa()}"
     
 class University: #Mert
     def __init__(self,id,name,courses=dict()):
+        self.id = id
+        self.name = name 
         students = {} #dict maps student_id -> Student object
         courses = {} #dict maps course_code -> Course object
 
